47355af81439cc509f56c4bd2d05b96833d68789
Christian <seannas04@gmail.com>
2019-01-30 17:43:09 +0200

    [Chore #163590944] Add Models

---
+++ src/models/parties.js
@@ -0,0 +1,16 @@
+const parties = [{
+    id: 1,
+    name: 'RPF',
+    hqAddress: 'Rusororo'
+  },
+  {
+    id: 2,
+    name: 'PDC',
+    hqAddress: 'Kacyiru'
+  },
+  {
+    id: 3,
+    name: 'PDI',
+    hqAddress: 'Nyarugenge'
+  }
+]
